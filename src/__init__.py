95d29500626ed9faec898b91e451da40b05fe9ad
sir-heed <sademolaadedeji@gmail.com>
2023-03-24 15:37:56 +0100

    Updated import

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,6 @@ import sys
 
 # Get the path to the current file
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 # Add the directory containing the protobuf-generated module to the sys.path
-protobuf_dir = os.path.join(current_dir, 'microservice_demo_with_grpc_shared_utils')
+protobuf_dir = os.path.join(current_dir, 'micorservice_demo_with_grpc_shared_utils')
 sys.path.append(protobuf_dir)
